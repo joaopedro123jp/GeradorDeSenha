d92d355edb4ee3d26289bbe1cc948e7ebba3b48a
joaopedro123jp <115241556+joaopedro123jp@users.noreply.github.com>
2024-08-16 12:09:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# ola, o codigo abaixo e um CRUED 
+
+from time import sleep
+
+
+
+
+def existe_arquivo(arquivo):
+    
+
+
+
+def Ler_arquivo():
+
+
+
+def cadastrar_pessoa()
+
+
+
+
+
+
+def LeiaInt(num):
+    while True:
+        try:
+            int(input(num))
+        except ValueError:
+            print("erro: informe um valor valido")
+            continue
+        else:
+            break
+
+
+
